b7953df833ffaab3406fb39e466518b844f336c9
Aza <diegogym@live.com>
2024-09-03 00:09:30 +0200

    refactor (?)

--- js/physics.js
+++ js/physics.js
@@ -2,6 +2,6 @@ export const gravity = 1;
 export const friction = 0.005;
 
 // export const physicsConstants = {
-//   gravity: 0.4,
-//   friction: 0.99,
+//   gravity: 1,
+//   friction: 0.005,
 // };
